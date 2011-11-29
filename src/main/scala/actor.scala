51a44247ef906cf678116bacbfb72d8ca792ffe8
Chris Lewis <chris@thegodcode.net>
2011-11-29 04:01:46 -0500

    actor stub

---
+++ src/main/scala/actor.scala
@@ -0,0 +1,17 @@
+package collabdraw
+
+import scala.actors.Actor
+
+/** Collaborative drawing has an obvious piece of shared mutable state:
+ *  the drawing! Actors can help here. A drawing actor should manage access
+ *  to current drawings; note that FetchDrawing should allow callers to
+ *  receive a Future... so respond!
+ */
+class DrawingActor extends Actor {
+  // ... and dont for get to start and stop me!
+  def act {}
+}
+
+/* Messages to handle; we'll need more.. */
+sealed trait DrawingMessage
+case class FetchDrawing(id: String)
