91a87bd972a8dd356852e46fd499899b5bb3cd37
Christopher League <league@contrapunctus.net>
2011-11-29 20:12:53 -0500

    part of actor, about to conflict

--- src/main/scala/actor.scala
+++ src/main/scala/actor.scala
@@ -1,6 +1,7 @@
 package collabdraw
 
 import scala.actors.Actor
+import scala.collection.mutable.HashMap
 
 /** Collaborative drawing has an obvious piece of shared mutable state:
  *  the drawing! Actors can help here. A drawing actor should manage access
@@ -8,10 +9,24 @@ import scala.actors.Actor
  *  receive a Future... so respond!
  */
 class DrawingActor extends Actor {
-  // ... and dont for get to start and stop me!
-  def act {}
+
+  val data = HashMap.empty[String, List[String]].withDefaultValue(Nil)
+
+  def act {
+    loop {
+      react {
+        case FetchDrawing(id) =>
+          reply(data(id).reverse.mkString)
+
+        case AddPath(id, p) =>
+          data.put(id, p :: data(id))
+      }
+    }
+  }
+
 }
 
 /* Messages to handle; we'll need more.. */
 sealed trait DrawingMessage
-case class FetchDrawing(id: String)
+case class FetchDrawing(id: String) extends DrawingMessage
+case class AddPath(id: String, p: String) extends DrawingMessage
